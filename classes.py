15d856da523b2f01f9358b5f9cf919233d0ffd26
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2023-03-29 10:24:00 +0300

    Documen, Cursor, Character classes, TestClasses

---
+++ classes.py
@@ -0,0 +1,120 @@
+""" Classes """
+
+class InctanceCreatingExeption(Exception):
+    """ This exeption ocures when there are some problems in inctance creation """
+
+class NotADocument(InctanceCreatingExeption):
+    """ This exeption ocures when """
+
+class UnaddableCharacter(Exception):
+    """ This exeption ocure when object passed in insert function can not be converted to string """
+
+class UnvalidCharacter(Exception):
+    """ This exeption ocures when unvalid character is passed in document """
+class EmptyDocDeleting(Exception):
+    """ This exeption ocures we try to delete character from empty document """
+
+class Document:
+    """ Document class """
+    def __init__ (self):
+        """ Init document """
+        self.characters = []
+        self.cursor = Cursor(self)
+        self.filename =  ''
+
+    def insert (self, character):
+        """ Insert character to document """
+        if not hasattr(character, "character"):
+            character = Character(character)
+        self.characters.append(character)
+        self.cursor.position = len(self.characters) - 1
+
+    def delete(self):
+        """ Delete character """
+        self.cursor.position = min(self.cursor.position, len(self.characters) - 1)
+        if len(self.characters) == 0:
+            raise EmptyDocDeleting
+        del self.characters[self.cursor.position]
+
+    def save(self):
+        """ Save document """
+        with open("test.txt", 'w') as file:
+            line = ""
+            for character in self.characters:
+                line += str(character)
+            file.write(line)
+
+    def forward(self):
+        """ Set cursor one step forward """
+        self.cursor.forward()
+
+    @property
+    def string(self):
+        """ Getter property which returns string representation of document characters """
+        return "".join((str(character) for character in self.characters))
+
+    def back(self):
+        """ Set cursor one step backward """
+        self.cursor.back()
+
+
+class Cursor:
+    """ Cursor class """
+    def __init__ (self, document):
+        """ Init cursor """
+        if not isinstance(document, Document):
+            raise NotADocument
+        self.document = document
+        self.position= 0
+
+    def forward(self):
+        """ Set cursor one step forward """
+        self.position += 1
+
+    def back(self):
+        """ Set cursor one step backward """
+        self.position -= 1
+
+    def home(self):
+        """ Set cursor home """
+        while self.document.characters[self.position-1].character != '\n':
+            self.position -= 1
+            if self.position == 0:
+                # Got to beginning of file before newline break
+                break
+
+    def end(self):
+        """ Set character to the end """
+        while self.position< len(self.document.characters)\
+        and self.document.characters[self.position].character != '\n':
+            self.position += 1
+
+class Character:
+    """ Character class """
+    def __init__(self, character,bold = False, italic = False, underline= False):
+        """ Init character """
+        if len(character) != 1:
+            raise UnvalidCharacter
+        try:
+            str(character)
+        except TypeError:
+            raise UnvalidCharacter
+        self.character = character
+        self.bold = bold
+        self.italic = italic
+        self.underline = underline
+
+    def __str__(self):
+        """ Convert object inctance to string """
+        bold = "*" if self.bold else ''
+        italic = "/" if self.italic else ''
+        underline = "_" if self.underline else ''
+        return bold + italic + underline + self.character
+
+document = Document()
+document.insert("h")
+document.insert("e")
+document.insert(Character('l', bold=True, italic=True))
+document.insert(Character('l', bold=True))
+document.insert("o")
+document.save()
---
+++ test_classes.py
@@ -0,0 +1,116 @@
+
+""" Test classes """
+import unittest
+from classes import Document, Character, Cursor,\
+NotADocument, UnvalidCharacter, EmptyDocDeleting
+
+class TestClasses(unittest.TestCase):
+    """ Test Document, Cursor and Character classes """
+    def test_inctance_creation(self):
+        """ Test inctance creation """
+        document = Document()
+        self.assertIsInstance(document, Document)
+        cursor = Cursor(document)
+        self.assertIsInstance(cursor, Cursor)
+        character = Character("a")
+        self.assertIsInstance(character, Character)
+
+        with self.assertRaises(NotADocument):
+            _ = Cursor(1)
+        with self.assertRaises(TypeError):
+            _ = Character(1, 2, 3)
+        with self.assertRaises(UnvalidCharacter):
+            _ = Character("asd")
+        with self.assertRaises(UnvalidCharacter):
+            _ = Character([])
+
+    def test_character_insertion(self):
+        """ Test character insertion """
+        document = Document()
+        document.insert("h")
+        document.insert("e")
+        document.insert(Character('l', bold=True))
+        document.insert(Character('l', bold=True))
+        document.insert("o")
+        document.insert("\n")
+        document.insert(Character('w', italic=True))
+        document.insert(Character('o', italic=True))
+        document.insert(Character('r', underline=True))
+        document.insert("l")
+        document.insert("d")
+
+        self.assertEqual(document.string, """he*l*lo\n/w/o_rld""")
+
+    def test_unvalid_character_insertion(self):
+        """ Test invalid character exeption """
+        document = Document()
+
+        with self.assertRaises(UnvalidCharacter):
+            document.insert(Character("test", bold=True))
+        with self.assertRaises(UnvalidCharacter):
+            document.insert("test")
+        with self.assertRaises(UnvalidCharacter):
+            document.insert([1, 2, 3])
+        with self.assertRaises(UnvalidCharacter):
+            document.insert(Character([1, 2, 3]))
+        with self.assertRaises(UnvalidCharacter):
+            document.insert([])
+
+    def test_cursor(self):
+        """ Test cursor """
+        document = Document()
+        document.insert("h")
+        document.insert("e")
+        document.insert(Character('l', bold=True))
+        document.insert(Character('l', bold=True))
+        document.insert("o")
+        document.insert("\n")
+        document.insert(Character('w', italic=True))
+        document.insert(Character('o', italic=True))
+        document.insert(Character('r', underline=True))
+        document.insert("l")
+        document.insert("d")
+
+        self.assertEqual(document.cursor.position, 10)
+        document.cursor.home()
+        self.assertEqual(document.cursor.position, 6)
+        document.characters[0].underline = True
+        self.assertEqual(document.string, """_he*l*lo\n/w/o_rld""")
+        document.cursor.home()
+        self.assertEqual(document.cursor.position, 6)
+        document.back()
+        document.cursor.home()
+        self.assertEqual(document.cursor.position, 0)
+        document.forward()
+        self.assertEqual(document.cursor.position, 1)
+        document.cursor.end()
+        self.assertEqual(document.cursor.position, 5)
+        document.delete()
+        self.assertEqual(document.string, """_he*l*lo/w/o_rld""")
+        document.save()
+
+    def test_deleting(self):
+        """ Test deleting """
+        document = Document()
+        document.insert("h")
+        document.insert("e")
+        document.insert(Character('l', bold=True))
+        document.insert(Character('l', bold=True))
+        document.insert("o")
+
+        self.assertEqual(document.string, """he*l*lo""")
+        document.delete()
+        self.assertEqual(document.string, """he*l*l""")
+        document.delete()
+        self.assertEqual(document.string, """he*l""")
+        document.delete()
+        self.assertEqual(document.string, """he""")
+        document.delete()
+        self.assertEqual(document.string, """h""")
+        document.delete()
+        self.assertEqual(document.string, """""")
+        with self.assertRaises(EmptyDocDeleting):
+            document.delete()
+
+if __name__ == "__main__":
+    unittest.main(argv=[''], verbosity=2, exit=False)
